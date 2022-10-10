d3da1d185d55ff77dd01972e7d730053aa81949b
laRayna <101940870+laRayna@users.noreply.github.com>
2022-10-10 12:30:13 -0500

    calendar
    Selecting dates

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+var MONTHS = [
+  "Janurary",
+  "Feburary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,6 +23,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime date = DateTime(2022, 10, 07);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,14 +55,43 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(children: [
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Text(
-            "Welcome!",
+            'Welcome!',
             style: TextStyle(
-              fontSize: 36,
+              fontSize: 56,
             ),
           ),
-        )
+        ),
+        SizedBox(height: 25),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                '${date.month}/${date.day}/${date.year}',
+                style: TextStyle(fontSize: 20),
+              ),
+              OutlinedButton.icon(
+                  icon: Icon(Icons.calendar_month, color: Colors.white),
+                  label: Text('Choose a different date',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                  onPressed: () async {
+                    DateTime? newDate = await showDatePicker(
+                      context: context,
+                      initialDate: date,
+                      firstDate: DateTime(2022),
+                      lastDate: DateTime(2024),
+                    );
+
+                    if (newDate == null) return;
+
+                    setState(() => date = newDate);
+                  }),
+            ],
+          ),
+        ),
       ]),
     );
   }
